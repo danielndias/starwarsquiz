ecf4aa281d58e0b5801dc84b62107d4c66555cf5
Daniel Dias <danielnunes@hotmail.com>
2018-07-15 14:39:31 -0400

    First Commit. Creating Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { PanelComponent } from './panel/panel.component';
+import { AttemptsComponent } from './attempts/attempts.component';
+import { ProgressComponent } from './progress/progress.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopComponent,
+    PanelComponent,
+    AttemptsComponent,
+    ProgressComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top/top.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-top',
+    templateUrl: './top.component.html',
+    styleUrls: ['./top.component.css']
+})
+
+export class TopComponent { }
